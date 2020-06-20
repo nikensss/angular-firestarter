ff992d6aec7f48b12fed5a6aedc0446dde72db0b
nikens-mbp16 <rikitzzz@gmail.com>
2020-06-20 22:07:40 +0200

    home page; firebase;
    Added a home page component. Created a firebase project to which
    this app is connected to now. Set-up the firebase config in
    environment.ts, installed 'firebase' as a dependency.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBmvUyd3WlDz39sKSg1HAcrnecGJ3tm6jI',
+    authDomain: 'firestarter-nikensss.firebaseapp.com',
+    databaseURL: 'https://firestarter-nikensss.firebaseio.com',
+    projectId: 'firestarter-nikensss',
+    storageBucket: 'firestarter-nikensss.appspot.com',
+    messagingSenderId: '154791293142',
+    appId: '1:154791293142:web:e256c932f2e56a5d769977',
+    measurementId: 'G-RL3BRY8V70'
+  }
 };
 
 /*

11fe8ce23259a0e046778de4e403c3e4a1eff58e
nikens-mbp16 <rikitzzz@gmail.com>
2020-06-24 22:36:47 +0200

    user module; google login;
    Created the user module. It will contain the login page, a router,
    a guard and some directives. Created also the google authentication
    directive so that any button can be used to trigger google login.
    Also created a logout button only visible if the user is logged in.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBmvUyd3WlDz39sKSg1HAcrnecGJ3tm6jI',
+    authDomain: 'firestarter-nikensss.firebaseapp.com',
+    databaseURL: 'https://firestarter-nikensss.firebaseio.com',
+    projectId: 'firestarter-nikensss',
+    storageBucket: 'firestarter-nikensss.appspot.com',
+    messagingSenderId: '154791293142',
+    appId: '1:154791293142:web:e256c932f2e56a5d769977',
+    measurementId: 'G-RL3BRY8V70'
+  }
 };
